a926e6da5638613d634d22a2f3e65419145c3f54
Dan Visel <dbvisel@gmail.com>
2013-07-29 11:56:17 -0400

    Create input.json.php

---
+++ input.json.php
@@ -0,0 +1,60 @@
+
+
+<?php
+header('Content-type: application/json');
+
+// this is from here: http://www.ravelrumba.com/blog/json-google-spreadsheets/
+ 
+// Set your CSV feed
+$feed = 'https://docs.google.com/spreadsheet/pub?key=XXXXXXX&single=true&gid=0&output=csv';
+ 
+// Arrays we'll use later
+$keys = array();
+$newArray = array();
+ 
+// Function to convert CSV into associative array
+function csvToArray($file, $delimiter) { 
+  if (($handle = fopen($file, 'r')) !== FALSE) { 
+    $i = 0; 
+    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
+      for ($j = 0; $j < count($lineArray); $j++) { 
+        $arr[$i][$j] = $lineArray[$j]; 
+      } 
+      $i++; 
+    } 
+    fclose($handle); 
+  } 
+  return $arr; 
+} 
+ 
+// Do it
+$data = csvToArray($feed, ',');
+ 
+// Set number of elements (minus 1 because we shift off the first row)
+$count = count($data) - 1;
+ 
+//Use first row for names  
+$labels = array_shift($data);  
+ 
+foreach ($labels as $label) {
+  $keys[] = $label;
+}
+ 
+// Add Ids, just in case we want them later
+$keys[] = 'id';
+ 
+for ($i = 0; $i < $count; $i++) {
+  $data[$i][] = $i;
+}
+ 
+// Bring it all together
+for ($j = 0; $j < $count; $j++) {
+  $d = array_combine($keys, $data[$j]);
+  $newArray[$j] = $d;
+}
+ 
+// Print it out as JSON
+echo '{"books":';
+echo json_encode($newArray);
+echo '}';
+?>
